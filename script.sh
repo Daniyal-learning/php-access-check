11f893aeeada88b160329d1a165680d56b474273
Daniyal-learning <78045149+Daniyal-learning@users.noreply.github.com>
2025-03-12 22:48:47 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Switching to sudo mode..."
+    exec sudo -i bash "$0" "$@"
+    exit
+fi
+
 CONFIG_DIR="/etc/nginx/sites-available"
 
 INCLUDE_LINE="include /etc/nginx/extras/wp_content_blocked.conf;"
@@ -7,7 +13,6 @@ INCLUDE_LINE="include /etc/nginx/extras/wp_content_blocked.conf;"
 echo "Checking for commented-out include lines..."
 
 for file in "$CONFIG_DIR"/*; do
-    # Check if it's a regular file
     if [ -f "$file" ]; then
         FOLDER_NAME=$(basename "$file")
         
